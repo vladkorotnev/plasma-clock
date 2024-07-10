8c7988d0bdbb4d3792bacd5ef9d8d1c88a278ea6
akasaka <akasaka@nezumi.local>
2024-07-10 23:46:22 +0900

    Two more melodies

--- include/sound/melodies.h
+++ include/sound/melodies.h
@@ -16,8 +16,10 @@ extern const melody_sequence_t bouken;
 extern const melody_sequence_t gentle_jena;
 extern const melody_sequence_t gammapolisz;
 extern const melody_sequence_t like_the_wind;
+extern const melody_sequence_t waiting_freqs;
+extern const melody_sequence_t the_way;
 
-static constexpr const char * all_chime_names = "Just Beep,PC-98 Boot Chime,Russ28 Notification Sound,Shtrikh-M POS Boot Chime,A.M. - Arise,Caramelldansen,BoA - Duvet,T-Square - Truth,Haruhi no Omoi,When Present Is Past,Hishoku no Sora,Bouken Desho Desho,Gentle Jena,Gammapolisz,Like The Wind (TMMS),Random";
+static constexpr const char * all_chime_names = "Just Beep,PC-98 Boot Chime,Russ28 Notification Sound,Shtrikh-M POS Boot Chime,A.M. - Arise,Caramelldansen,BoA - Duvet,T-Square - Truth,Haruhi no Omoi,When Present Is Past,Hishoku no Sora,Bouken Desho Desho,Gentle Jena,Gammapolisz,Like The Wind (TMMS),NightRadio - Waiting Freqs,NightRadio - The Way,Random";
 static const melody_sequence_t all_chime_list[] = {
     just_beep,
     pc98_pipo,
@@ -34,5 +36,7 @@ static const melody_sequence_t all_chime_list[] = {
     gentle_jena,
     gammapolisz,
     like_the_wind,
+    waiting_freqs,
+    the_way,
 };
 static const int all_chime_count = sizeof(all_chime_list)/sizeof(melody_sequence_t);
