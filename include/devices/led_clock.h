80f0d00a4c3d94d85fe8aa937aee1ad6cd2032af
akasaka <akasaka@nezumi.local>
2024-11-07 20:38:06 +0900

    LED edition version bump

--- include/devices/led_clock.h
+++ include/devices/led_clock.h
@@ -7,11 +7,10 @@
 #define HAS_KEYPAD
 #define HAS_VARYING_BRIGHTNESS
 #define HAS_LIGHT_SENSOR
-// #define HAS_OTAFVU
 
 // Plasma Information System OS (not DOS, there's no disk in it!)
 #define PRODUCT_NAME "LePIS-OS"
-#define PRODUCT_VERSION "5"
+#define PRODUCT_VERSION "5.1"
 
 // ---- Connection to beeper ----
 const gpio_num_t HWCONF_BEEPER_GPIO = GPIO_NUM_33;
