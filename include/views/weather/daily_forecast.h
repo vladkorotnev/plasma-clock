840518a8ac83435136bab1a51cb22cfff0aaf7a6
akasaka <akasaka@nezumi.local>
2024-12-13 21:06:34 +0900

    Dynamically size weather forecast screen

--- include/views/weather/daily_forecast.h
+++ include/views/weather/daily_forecast.h
@@ -1,17 +1,23 @@
 #pragma once
 #include <views/framework.h>
+#include <vector>
 
 class DailyForecastView: public Screen {
 public:
     bool controllable;
     DailyForecastView(bool controllable = false);
-    ~DailyForecastView();
     void prepare() override;
+    void render(FantaManipulator*) override;
     void step() override;
 
 private:
     class Column;
-    Column * leftView;
-    Column * rightView;
+    std::vector<Column *> columns;
     int offset;
+    int total_per_screen;
+    int one_widget_width;
+    int screen_width;
+    bool need_width_update;
+    void update_width();
+    void update_data();
 };
